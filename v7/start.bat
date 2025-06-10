87b58ca1bc0e8e3815a32e92fa871b713620f886
jayyuan <2218773049@qq.com>
2025-06-10 17:59:10 +0800

    windows bat

--- v7/start.bat
+++ v7/start.bat
@@ -1,5 +1,7 @@
 @echo off
-start "" "./v7.exe" "nuoshou776" "Yuan970901" --num100 20 --num200 10 --num500 16 --num1000 16 --num2000 1
-::start "" "./v7.exe" "账号1" "密码" --num100 20 --num200 10 --num500 16 --num1000 16 --num2000 1
-::start "" "./v7.exe" "账号1" "密码" --num100 20 --num200 10 --num500 16 --num1000 16 --num2000 1
+::有3w的账号
+start "" "./v7.exe" "账号1" "密码" --is3w --num100 20 --num200 10 --num500 16 --num1000 16 --num2000 1
+start "" "./v7.exe" "账号2" "密码" --is3w --num100 20 --num200 10 --num500 16 --num1000 16 --num2000 1
+::没有3w的账号
+start "" "./v7.exe" "账号2" "密码" --num100 0 --num200 0 --num500 0 --num1000 0 --num2000 10
 echo 所有程序已启动
