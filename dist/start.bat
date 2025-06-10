ac535cad6afd7c16bc290cd64aba7f1c44595385
Administrator <2218773049@qq.com>
2025-06-10 20:52:50 +0800

    11

--- dist/start.bat
+++ dist/start.bat
@@ -1,4 +1,5 @@
 @echo off
-start "" "./v7.exe" "账号1" "密码" --is3w --num100 20 --num200 10 --num500 16 --num1000 16 --num2000 1
-start "" "./v7.exe" "账号2" "密码" --is3w --num100 20 --num200 10 --num500 16 --num1000 16 --num2000 1
+start "" "./v7.exe" "账号" "密码" --is3w --num100 20 --num200 10 --num500 16 --num1000 16 --num2000 1
+start "" "./v7.exe" "账号" "密码" --is3w --num100 20 --num200 10 --num500 16 --num1000 16 --num2000 1
+start "" "./v7.exe" "账号" "密码" --is3w --num100 0 --num200 0 --num500 0 --num1000 0 --num2000 10
 echo 所有程序已启动
--- start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-start "" "./v6.exe" "账号1" "密码"
-start "" "./v6.exe" "账号2" "密码"
-start "" "./v6.exe" "账号3" "密码"
-echo 所有程序已启动
